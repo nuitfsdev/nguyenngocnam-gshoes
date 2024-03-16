b917392f83080d9f3d4db5d5d067ca5928a77a90
nuitfsdev <20521631@gm.uit.edu.vn>
2024-03-17 01:03:06 +0700

    save local storage

--- src/pages/ShoppingCart/ShoppingCart.js
+++ src/pages/ShoppingCart/ShoppingCart.js
@@ -1,5 +1,5 @@
 import Product from "./Components/Product/Product";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import listItem from "./Components/Product/shoes";
 import Cart from "./Components/Cart/Cart";
 import './ShoppingCart.css';
@@ -7,14 +7,26 @@ import './ShoppingCart.css';
 function ShoppingCart()
 {
     const [listCartItem, setListCartItem] = useState([]);
+
+    useEffect(() => {
+        const savedData = localStorage.getItem('cartData');
+        if (savedData) {
+            setListCartItem(JSON.parse(savedData));
+        }
+      }, []);
+    
+
     const addToCart = (product) => {
         setListCartItem([...listCartItem, {product, quantity: 1}]);
+        localStorage.setItem('cartData', JSON.stringify([...listCartItem, {product, quantity: 1}]));
     }
     const deleteFromCart = (item) => {
         const index = listCartItem.findIndex((cartItem) => cartItem.product.id === item.product.id);
         if (index > -1) {
             listCartItem.splice(index, 1);
             setListCartItem([...listCartItem]);
+            localStorage.setItem('cartData', JSON.stringify([...listCartItem]));
+           
         }
     }
     const updateFromCart = (item) => {
@@ -22,6 +34,7 @@ function ShoppingCart()
         if (index > -1) {
             listCartItem[index].quantity = item.quantity;
             setListCartItem([...listCartItem]);
+            localStorage.setItem('cartData', JSON.stringify([...listCartItem]));
         }
     }
     const checkExistProductInCart = (product) => {
